87f96ab3de52a6888b06bfe4187634cb10e2d925
Julian Henry <julianplushenry@gmail.com>
2019-09-05 23:49:06 -0400

    Added Conversion from JS to Python Dict

--- type_getter.py
+++ type_getter.py
@@ -3,47 +3,42 @@ import pandas as pd
 
 showdata = pd.read_csv('data.csv')
 
-test = '''{chansey: {
-		num: 113,
-		species: "Chansey",
-		types: ["Normal"],
-		gender: "F",
-		baseStats: {hp: 250, atk: 5, def: 5, spa: 35, spd: 105, spe: 50},
-		abilities: {0: "Natural Cure", 1: "Serene Grace", H: "Healer"},
-		heightm: 1.1,
-		weightkg: 34.6,
-		color: "Pink",
-		prevo: "happiny",
-		evoType: "levelHold",
-		evoItem: "Oval Stone",
-		evoCondition: "during the day",
-		evos: ["blissey"],
-		eggGroups: ["Fairy"],
-		canHatch: true,
-	}'''
+pokedex_js = ''
+
+with open('pokedex.js', 'r+') as f:
+    pokedex_js = f.read()
+
 def insert (source_str, insert_str, pos):
     return source_str[:pos]+insert_str+source_str[pos:]
+
+def find(s, ch):
+    return [i for i, ltr in enumerate(s) if ltr == ch]
 
 def javascript_dict_to_python(s): #TODO address boolean format, address errant quotes
     within_word = False
     s = s.replace('\"','')
-    quote_indices = []
+    
+    single_quote_i = []
     for i in range(0,len(s)-1): 
         if s[i+1].isalpha() and (not within_word):
-            quote_indices.append(i+1)
+            single_quote_i.append(i+1)
             within_word = not within_word
         elif (not s[i+1].isalpha()) and within_word:
-            quote_indices.append(i+1)
+            single_quote_i.append(i+1)
             within_word = not within_word
-    for i in reversed(quote_indices): s = insert(s, '\'', i)
-    for i in range(len(quote_indices[:-1])):
-        print(s[quote_indices[i]:quote_indices[i+1]])
+    for i in reversed(single_quote_i): s = insert(s, '\'', i)
+    
+    quotes = find(s, '\'')
+    remove_space_i = []
+    for i in range(len(quotes)-1):
+        if (s[quotes[i]+1:quotes[i+1]] == ' '):
+            remove_space_i.append([quotes[i],quotes[i+1]])
+    for rm in reversed(remove_space_i):
+        s = f"{s[:rm[0]]} {s[rm[1]+1:]}"
     return s
     
-print(javascript_dict_to_python(test)) 
+print(javascript_dict_to_python(pokedex_js)) 
 
 # f = open('pokedex_dict.txt', 'w+')
 # f.write(js_dex)
 # f.close
-# with open('pokedex_dict.txt', 'r') as f:
-#     data_dict = exec(f.read)
