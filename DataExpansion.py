bcd7d5fb163b396dac46dd79e51a78f51e715b79
juleshenry <julianplushenry@gmail.com>
2019-09-21 20:29:20 -0400

    Initial Type Metric Script

--- DataExpansion.py
+++ DataExpansion.py
@@ -15,6 +15,7 @@ df = df.drop(['battle_url'],axis=1)
 df = df.dropna()
 df = df[df.elo != 2019]
 df = df[df.num_turns > 15]
+
 #Aggregate Team Base Stats
 stats = [pokedex[da.pokekey(p)]['baseStats'] for p in da.destring(df.team_1.iloc[0])]
 agg_stats = {}
@@ -25,26 +26,60 @@ for i in stats:
         else:
             agg_stats.update({j:agg_stats.get(j) + i.get(j)})
 
-#Calculate Distribution of Pokemon
-agg_roster = []
-for i in range(2000):
-    agg_roster += [mon for mon in da.destring(df.team_1.iloc[i])]
-    agg_roster += [mon for mon in da.destring(df.team_2.iloc[i])]
-roster_freq = {}
-for mon in agg_roster:
-    if(mon not in roster_freq.keys()):
-        roster_freq.update({mon:1})
-    else:
-        roster_freq.update({mon:roster_freq.get(mon)+1})
-dict_roster_freq = collections.OrderedDict(sorted(roster_freq.items(),key = lambda kv: kv[1]))
-#Type Effectiveness Metric
-samplesize = 0
-freq_key_list = list(dict_roster_freq.keys())
-freq_val_list = list(dict_roster_freq.values())
-for freq in freq_val_list: samplesize += freq
+t1 = [mon for mon in da.destring(df.team_1.iloc[100])]
+t2 = [mon for mon in da.destring(df.team_2.iloc[100])]
+t1_types = [pokedex.get(da.pokekey(mon)).get('types') for mon in t1]
+t2_types = [pokedex.get(da.pokekey(mon)).get('types') for mon in t2]
 
-for i in range(40):
-    print(f"{i+1}. {freq_key_list[-i-1]} : {freq_val_list[-i-1]*100/samplesize}%")
+print(str(t1)+' VS. '+str(t2))
+t1_metric = 0
+
+for mon1 in t1_types:
+    for att_type in mon1:
+        for mon2 in t2_types:
+            if(len(mon2)==1): 
+                t1_metric += np.log2(da.attack_effectiveness(att_type,mon2[0],mon2[0]))
+            else:
+                t1_metric += np.log2(da.attack_effectiveness(att_type,mon2[0],mon2[1]))
+
+for mon2 in t2_types:
+    for att_type in mon2:
+        for mon1 in t1_types:
+            if(len(mon1)==1): 
+                t1_metric -= np.log2(da.attack_effectiveness(att_type,mon1[0],mon1[0]))
+            else:
+                t1_metric -= np.log2(da.attack_effectiveness(att_type,mon1[0],mon1[1]))
+
+print(t1_metric)
+#for the types, calculates 36+ combos
+#produce a single number
+
+
+
+
+#Calculate Distribution of Pokemon
+# agg_roster = []
+# for i in range(2000):
+#     agg_roster += [mon for mon in da.destring(df.team_1.iloc[i])]
+#     agg_roster += [mon for mon in da.destring(df.team_2.iloc[i])]
+# roster_freq = {}
+# for mon in agg_roster:
+#     if(mon not in roster_freq.keys()):
+#         roster_freq.update({mon:1})
+#     else:
+#         roster_freq.update({mon:roster_freq.get(mon)+1})
+# dict_roster_freq = collections.OrderedDict(sorted(roster_freq.items(),key = lambda kv: kv[1]))
+# samplesize = 0
+# freq_key_list = list(dict_roster_freq.keys())
+# freq_val_list = list(dict_roster_freq.values())
+# for freq in freq_val_list: samplesize += freq
+# for i in range(40):
+#     print(f"{i+1}. {freq_key_list[-i-1]} : {freq_val_list[-i-1]*100/samplesize}%")
+#Type Effectiveness Metric
+# print(da.attack_effectiveness('Fire','Grass','Ice'))
+
+
+
 
 # plt.bar(dict_roster_freq.keys(),dict_roster_freq.values(),color='g')
 # plt.xticks(rotation=90)
